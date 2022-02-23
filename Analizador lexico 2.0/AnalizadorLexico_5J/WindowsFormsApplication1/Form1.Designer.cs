2818adec5e11baff7f82cb47b4785517a28f65fa
FutlessMercury <84038220+FutlessMercury@users.noreply.github.com>
2022-02-22 19:27:22 -0600

    Add files via upload

---
+++ Analizador lexico 2.0/AnalizadorLexico_5J/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.btnanalizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.RichTextBox();
+            this.txtSalida = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(419, 297);
+            this.btnlimpiar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(99, 33);
+            this.btnlimpiar.TabIndex = 0;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(525, 297);
+            this.btnsalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(77, 33);
+            this.btnsalir.TabIndex = 1;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // btnanalizar
+            // 
+            this.btnanalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnanalizar.Location = new System.Drawing.Point(191, 297);
+            this.btnanalizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnanalizar.Name = "btnanalizar";
+            this.btnanalizar.Size = new System.Drawing.Size(220, 33);
+            this.btnanalizar.TabIndex = 2;
+            this.btnanalizar.Text = "Analizador Lexico";
+            this.btnanalizar.UseVisualStyleBackColor = true;
+            this.btnanalizar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escribe Tu Codigo fuente:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 347);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(386, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Salida de Tokens Resultantes:";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEntrada.Location = new System.Drawing.Point(16, 54);
+            this.txtEntrada.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(577, 234);
+            this.txtEntrada.TabIndex = 6;
+            this.txtEntrada.Text = "";
+            this.txtEntrada.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalida.Location = new System.Drawing.Point(24, 394);
+            this.txtSalida.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(577, 240);
+            this.txtSalida.TabIndex = 7;
+            this.txtSalida.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 650);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnanalizar);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnlimpiar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Analizador léxico versión 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button btnanalizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtEntrada;
+        private System.Windows.Forms.RichTextBox txtSalida;
+    }
+}
+
---
+++ Analizador lexico 2.0/AnalizadorLexico_5J/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //   Token1.     Palabra reservada   int, float, double, ....
+            //   Token2.     Identificador (variable)
+            //   Token3.     Numero entero
+            //   Token4.     Numero con punto decimal
+            //   Token5.     Operador Aritmetico   +  -  *   /  ^   
+            //   Token6.     Operador Relacional   >  >=  <   <=   !=   ==
+            //   Token7.     Operador Logico   &&    ||   ~
+            //   Token8.     Asignacion    =
+            //   Token9.     Fin de instruccion     ;
+            //   Token10.    Separador    ,
+            //   Token11.    Parentesis   (   )
+            //   Token12.    Corchetes    [   ]
+            //   Token13.    Llaves       {   }
+            //   Token14.    Cadena       "Hola"
+            //   Token15.    Caracter     'z'
+            //   Token16.    dos puntos  :
+            //   Token17.    incremento   ++
+            //   Token18.    Decremento   --
+            //   Token19.    Operador aritmetivo inclusivo    +=  -=  *=  /=
+            //   Token20.    Punto   .
+
+            //Equipo   1    Tokens   3, 9, 11, 13, 18      Juan José Baños.        4:45.  PM
+
+            //VARIABLES UTILIZADAS
+           string patron2 = @"[a-zA-Z]\w*";      //   Identicador  (Variable
+            string patron4 = @"[\+-]?\d+\.\d+";   //   Numero con punto decimal
+            string patron3 = @"[\+-]?\d+";        //   numero entero
+            string patron8 = "=";                 //   Asignacion  =
+            string patron20 = ";";                //   Punto y coma
+            string patron16 = ":";                 //dos puntos
+            string patron10 = ",";                  //coma
+            string patron17 = @"[\+]{2}\w*";       //incremento
+            string patron18 = @"[\-]{2}\w*";       //decremento
+            string patron21 = @"[\\.]\w*";       //punto
+            string patron5 = @"[\+|\-|\*|\/|\^]\w*";       //Operador Aritmetico
+            string patron11 = @"[\\(|\\)]\w*";       //Parentesis
+            string patron12 = @"[\\[|\\\]]\w*";       //Corchetes
+            string patron13 = @"[\\{|\\}]\w*";       //Llaves
+            String patron6 = @"[a-zA-Z]\w*+(int|float)"; //operadores relacionales
+
+            string patron = patron2 + "|" + patron4 + "|" + patron3 + "|" + patron8 + "|" + patron20 + "|" + patron10 + "|" + patron16 + "|" + patron17 + "|" + patron18 + "|" + patron21 + "|" + patron5 + "|" + patron11 + "|" + patron12 + "|" + patron13 + "|" + patron6;
+            Regex exp2 = new Regex(patron2);
+            Regex exp4 = new Regex(patron4);
+            Regex exp3 = new Regex(patron3);
+            Regex exp8 = new Regex(patron8);
+            Regex exp20 = new Regex(patron20);
+            Regex exp16 = new Regex(patron16);
+            Regex exp10 = new Regex(patron10);
+            Regex exp17 = new Regex(patron17);
+            Regex exp18 = new Regex(patron18);
+            Regex exp21 = new Regex(patron21);
+            Regex exp5 = new Regex(patron5);
+            Regex exp11 = new Regex(patron11);
+            Regex exp12 = new Regex(patron12);
+            Regex exp13 = new Regex(patron13);
+            Regex exp6 = new Regex(patron6);
+
+
+            string signo;
+            string salida = string.Empty;
+            string cadena = txtEntrada.Text;
+            Match match = Regex.Match(cadena, patron);
+
+            while (match.Success)
+            {
+                signo = "positivo";
+                if (exp2.IsMatch(match.Value))
+                    salida += "Token 2: Identificador (Variable) ---> " + match.Value + "\r\n";
+                else if (exp4.IsMatch(match.Value))
+                {
+                    if (match.Value[0] == '-') signo = "negativo";
+                    salida += "Token 4: Numero decimal " + signo + " ---> " + match.Value + "\r\n";
+                }
+                else if (exp3.IsMatch(match.Value))
+                {
+                    if (match.Value[0] == '-') signo = "negativo";
+                    salida += "Token 3: Numero entero " + signo + " ---> " + match.Value + "\r\n";
+                }
+                else if (exp8.IsMatch(match.Value))
+                    salida += "Token 8: Igualdad ---> " + match.Value + "\r\n";
+                else if (exp20.IsMatch(match.Value))
+                    salida += "Token 20: Punto y coma ---> " + match.Value + "\r\n";
+                else if (exp16.IsMatch(match.Value))
+                    salida += "Token 16:Dos puntos ---> " + match.Value + "\r\n";
+                 else if (exp10.IsMatch(match.Value))
+                    salida += "Token 10: Coma ---> " + match.Value + "\r\n";
+                else if (exp17.IsMatch(match.Value))
+                    salida += "Token 17: Incremento ---> " + match.Value + "\r\n";
+                else if (exp18.IsMatch(match.Value))
+                    salida += "Token 18: Decremento ---> " + match.Value + "\r\n";
+                else if (exp21.IsMatch(match.Value))
+                    salida += "Token 21: Punto ---> " + match.Value + "\r\n";
+                else if (exp5.IsMatch(match.Value))
+                    salida += "Token 5: Operador Aritmetico ---> " + match.Value + "\r\n";
+                else if (exp11.IsMatch(match.Value))
+                    salida += "Token 11: Parentesis ---> " + match.Value + "\r\n";
+                else if (exp12.IsMatch(match.Value))
+                    salida += "Token 12: Corchetes ---> " + match.Value + "\r\n";
+                else if (exp13.IsMatch(match.Value))
+                    salida += "Token 13: Llaves ---> " + match.Value + "\r\n";
+                else if (exp6.IsMatch(match.Value))
+                    salida += "Token 6: Operadores relacionales ---> " + match.Value + "\r\n";
+                match = match.NextMatch();
+            }
+            txtSalida.Text = salida;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtEntrada.Clear();
+            txtSalida.Clear();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+           Application.Exit();
+        }
+    }
+}
